3fb201b1a02f90a4e0ba9b54d7607c551ddcca67
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2022-09-15 16:46:42 +0700

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,16 +1,25 @@
 require('dotenv').config()
 
-const Hapi = require('@hapi/hapi');
+const Hapi = require('@hapi/hapi')
+const Jwt = require('@hapi/jwt')
+
 const notes = require('./api/notes')
 const NotesService = require('./services/postgres/NotesService')
 const NotesValidator = require('./validator/notes')
+
 const users = require('./api/users')
 const UsersService = require('./services/postgres/UsersService')
 const UsersValidator = require('./validator/users')
 
+const authentications = require('./api/authentications')
+const AuthenticationsService = require('./services/postgres/AuthenticationsService')
+const TokenManager = require('./tokenize/TokenManager')
+const AuthenticationsValidator = require('./validator/authentications')
+
 const init = async () => {
   const notesService = new NotesService()
   const usersService = new UsersService()
+  const authenticationsService = new AuthenticationsService()
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -20,7 +29,29 @@ const init = async () => {
         origin: ['*'],
       },
     },
-  });
+  })
+
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ])
+
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id
+      }
+    })
+  })
 
   await server.register([
     {
@@ -35,6 +66,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator
+      }
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator
       }
     }
   ])
