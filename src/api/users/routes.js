2f314b96373b6c4f28457131a0b0cc2e027ea9ca
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2022-09-15 23:49:03 +0700

    collaboration implemented

--- src/api/users/routes.js
+++ src/api/users/routes.js
@@ -8,6 +8,11 @@ const routes = (handler) => [
         method: 'GET',
         path: '/users/{id}',
         handler: handler.getUserByIdHandler
+    },
+    {
+        method: 'GET',
+        path: '/users',
+        handler: handler.getUsersByUsernameHandler
     }
 ]
 
