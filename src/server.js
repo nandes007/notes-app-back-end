91e6e62848cdd6541b8bb612f251ce4b724b87f5
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2022-09-21 12:24:01 +0700

    S3 storage implemented

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const Hapi = require('@hapi/hapi')
 const Jwt = require('@hapi/jwt')
 const Inert = require('@hapi/inert')
-const path = require('path')
 
 const notes = require('./api/notes')
 const NotesService = require('./services/postgres/NotesService')
@@ -27,7 +26,7 @@ const ProducerService = require('./services/rabbitmq/ProducerService')
 const ExportsValidator = require('./validator/exports')
 
 const uploads = require('./api/uploads')
-const StorageService = require('./services/storage/StorageService')
+const StorageService = require('./services/S3/StorageService')
 const UploadsValidator = require('./validator/uploads')
 
 const init = async () => {
@@ -35,7 +34,7 @@ const init = async () => {
   const notesService = new NotesService(collaborationsService)
   const usersService = new UsersService()
   const authenticationsService = new AuthenticationsService()
-  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'))
+  const storageService = new StorageService()
 
   const server = Hapi.server({
     port: process.env.PORT,
