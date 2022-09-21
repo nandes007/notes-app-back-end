fe601f47d89d02282d741c75cf565e04c904f00e
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2022-09-21 10:14:37 +0700

    local storage implemented

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const Hapi = require('@hapi/hapi')
 const Jwt = require('@hapi/jwt')
+const Inert = require('@hapi/inert')
+const path = require('path')
 
 const notes = require('./api/notes')
 const NotesService = require('./services/postgres/NotesService')
@@ -24,11 +26,16 @@ const _exports = require('./api/exports')
 const ProducerService = require('./services/rabbitmq/ProducerService')
 const ExportsValidator = require('./validator/exports')
 
+const uploads = require('./api/uploads')
+const StorageService = require('./services/storage/StorageService')
+const UploadsValidator = require('./validator/uploads')
+
 const init = async () => {
   const collaborationsService = new CollaborationsService()
   const notesService = new NotesService(collaborationsService)
   const usersService = new UsersService()
   const authenticationsService = new AuthenticationsService()
+  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'))
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -43,6 +50,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt
+    },
+    {
+      plugin: Inert
     }
   ])
 
@@ -99,6 +109,13 @@ const init = async () => {
       options: {
         service: ProducerService,
         validator: ExportsValidator
+      }
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator
       }
     }
   ])
